e4c6dd294a4895137cb07ef74d7bc95036230a56
amirmrf882 <amirmaruf6605@gmail.com>
2025-03-21 15:46:10 +0700

    mengakses data

--- data.js
+++ data.js
@@ -35,9 +35,9 @@ console.log(pemudaWonocolo)
 
 // 4. Object (data yg mempunyai properti) > kurung kurawal
 const sabun = {
-    nama: 'lux'
-    jenis: 'cair'
-    ukuran: '100ml'
+    nama: 'lux',
+    jenis: 'cair',
+    ukuran: '100ml',
     harga: 30000
 }
 
@@ -46,3 +46,56 @@ const sabun = {
 
 // 6. null (himpunan/ data kosong)
 const desa = null
+
+// Mengakses data 
+// 1. string -> dengan menggunkan kurung siku diawal dimulai dari 0
+const meja = 'Kayu Jati'
+
+console.log(meja[5])
+console.log(meja.length)// menghitung panjang, panjang dari 1
+
+// mengakses data terakhir
+console.log(meja[meja.length - 1])
+
+// 2. number -> cara mengakses -> operasi math -> dijadikan string
+const hargaMinyak = 17500
+
+// diubah jadi string -> tinggal dikasih -> .toString() atau `${}`
+
+console.log('ribuan', hargaMinyak.toString()[1]);
+console.log('ratusan', `${hargaMinyak}`[2]);
+
+const angkaPuluhanribu = Math.floor(hargaMinyak/10000);
+console.log('puluhanribu', angkaPuluhanribu)
+
+const ribuan = hargaMinyak - angkaPuluhanribu * 10000;
+const angkaRibuan = Math.floor(ribuan/1000);
+console.log('ribuan', angkaRibuan );
+
+const ratusan = hargaMinyak - (angkaPuluhanribu * 10000 + angkaRibuan * 1000);
+const angkaRatusan = Math.floor(ratusan/100);
+console.log('ratusan', angkaRatusan)
+
+// modulo = sisa pembagian (menggunkan %)
+console.log('puluhan', Math.floor(hargaMinyak % 100/10))
+
+// 3. array -> cara mengakses dgn [] -> dimulai dari 0
+const buahFavorit = ['apel', 'pisang', 'durian']
+
+console.log(buahFavorit[2])
+
+// menambahkan data ke array
+// a. push -> menambahkan dibelakang
+buahFavorit.push('jeruk')
+
+// 4. Object-> dengan menuliskan nama objek disertai titik (.)
+
+console.log('Nama sabun:', sabun.nama)
+
+// menambahkan properti baru
+sabun.pembuat = {
+    namaPT: 'PT Wings',
+    alamat: 'prembun'
+}
+
+console.log(sabun.pembuat.namaPT)
