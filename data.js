1b380707c67fce2353cf717f593fbfd8a6016043
amirmrf882 <amirmaruf6605@gmail.com>
2025-03-21 14:44:39 +0700

    membahas jenis data

--- data.js
+++ data.js
@@ -1,1 +1,48 @@
-// jajal
+// belajar basic data
+// 1. membuat variable (const, let)
+// const suatu data tidak dapat diubah lagi
+// let suatu data dapat/yg bisa diubah lagi
+const nama = 'aup'
+let alamat = 'wonocolo'
+
+console.log(nama)
+console.log(alamat)
+
+alamat = 'prembun'
+console.log(alamat)
+
+// nama ='amir hafiz' keterangan akan terjadi error jika di run
+// console.log(nama)
+
+// jenis data
+// 1. string (kalimat, kata, huruf)
+// harus dibungkus dgn menggunakan ',",`
+const masjid = "nurul hid'yah"
+const nomorrumah = '3'
+
+console.log (nomorrumah + 2)
+
+// 2. number (angka yg bisa dioperasikan menggunakan kaidah matematika)
+const jumlahPenduduk = 2005
+
+console.log (jumlahPenduduk - 2)
+console.log (Math.floor(jumlahPenduduk/5)) //pembulatan bilangan ke bawah, ceil ke atas
+
+// 3. array (kumpulan beberapa data) >kurung siku
+const pemudaWonocolo = ['amir', 'anggita', nama ]
+
+console.log(pemudaWonocolo)
+
+// 4. Object (data yg mempunyai properti) > kurung kurawal
+const sabun = {
+    nama: 'lux'
+    jenis: 'cair'
+    ukuran: '100ml'
+    harga: 30000
+}
+
+// 5. undefined( data yang belom dibuat)
+// akan mencari data sabur tp property kemasan
+
+// 6. null (himpunan/ data kosong)
+const desa = null
